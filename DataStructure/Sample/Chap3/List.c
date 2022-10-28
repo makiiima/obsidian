cf7ec20d982d326fed374e053a23b0453c5d4afb
YunfanLi_Ubuntu <leemakima@gmail.com>
2022-10-28 22:58:40 +0800

    sample list

--- DataStructure/Sample/Chap3/List.c
+++ DataStructure/Sample/Chap3/List.c
@@ -1,20 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct Node;
 #define ElementType int
 typedef struct Node *PtrToNode;
 typedef PtrToNode List;
 typedef PtrToNode Position;
 
-struct Node{
-	ElementType Element;
-	Position Next;
+struct Node
+{
+    ElementType Element;
+    Position Next;
 };
-List MakeEmpty( List L );
-int IsEmpty( List L);
-int IsLast( Position P, List L );
-Position Find( ElementType X, List L ); 
-void Delete( ElementType X, List L );
+List MakeEmpty(List L);
+int IsEmpty(List L);
+int IsLast(Position P, List L);
+Position Find(ElementType X, List L);
+void Delete(ElementType X, List L);
 Position FindPrevious(ElementType X, List L);
 void Insert(ElementType X, List L, Position P);
 void DeleteList(List L);
@@ -25,61 +26,78 @@ ElementType Retrieve(Position P);
 
 int IsEmpty(List L)
 {
-  return L->Next==NULL;
+    return L->Next == NULL;
 }
 
-int IsLast(Position P,List L)
+int IsLast(Position P, List L)
 {
-  return P->Next==NULL;
+    return P->Next == NULL;
 }
 
-Position Find(ElementType X,List L)
+Position Find(ElementType X, List L)
 {
-  Position P;
-  P=L->Next;
-  while(P!=NULL&&P->Element!=X)
-    P=P->Next;
-  if (P->Next==NULL&&P->Element!=X)
-    return NULL;
-  return P;
+    Position P;
+    P = L->Next;
+    while (P != NULL && P->Element != X)
+        P = P->Next;
+    if (P->Next == NULL && P->Element != X)
+        return NULL;
+    return P;
 }
 
-void Delete(ElementType X,List L)
+void Delete(ElementType X, List L)
 {
-  Position P,TmpCell;
-  P=FindPrevious(X,L);
-  if(!IsLast(P,L))
-  {
-    TmpCell=P->Next;
-    P->Next=TmpCell->Next;
-    free(TmpCell);
-  }
+    Position P, TmpCell;
+    P = FindPrevious(X, L);
+    if (!IsLast(P, L))
+    {
+        TmpCell = P->Next;
+        P->Next = TmpCell->Next;
+        free(TmpCell);
+    }
 }
 
-Position FindPrevious(ElementType X,List L)
+Position FindPrevious(ElementType X, List L)
 {
-  Position P;
-  P=L;
-  while(P->Next->Element!=X&&P->Next!=NULL)
-    P=P->Next;
-  if(P->Next==NULL)
-    return NULL;
-  return P;
+    Position P;
+    P = L;
+    while (P->Next->Element != X && P->Next != NULL)
+        P = P->Next;
+    if (P->Next == NULL)
+        return NULL;
+    return P;
 }
 
 void Insert(ElementType X, List L, Position P)
 {
-  Position TmpCell;
-  TmpCell=malloc(sizeof(struct Node));
-  if(TmpCell==NULL)
-    FatalError("Out of space!");
+    Position TmpCell;
+    TmpCell = malloc(sizeof(struct Node));
+    if (TmpCell == NULL)
+        FatalError("Out of space!");
 
-  TmpCell->Element=X;
-  TmpCell->Next=P->Next;
-  P->Next=TmpCell;
+    TmpCell->Element = X;
+    TmpCell->Next = P->Next;
+    P->Next = TmpCell;
+}
+
+void DeleteList(List L)
+{
+    Position p;
+    p=L;
+    while(L)
+    {
+        free(p);
+        L=L->Next;
+        p=L;
+    }
+}
+
+Position Header(List L)
+{
+    
 }
 
 int main()
 {
-  return 0;
+    return 0;
 }
