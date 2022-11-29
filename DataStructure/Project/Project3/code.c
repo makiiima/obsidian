3e3a3020eecacc89013ba578366f66e520b45dcd
YunfanLi_Ubuntu <leemakima@gmail.com>
2022-11-29 16:32:35 +0800

    ds proj3 half

--- DataStructure/Project/Project3/code.c
+++ DataStructure/Project/Project3/code.c
@@ -1,29 +1,61 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 // #define DEBUG
 
+#define INF 0x7fffffff
 #define MAXSIZE 1000
+
+#include"queue.h"
+
+
 int s[MAXSIZE][MAXSIZE];
 int d[MAXSIZE][MAXSIZE];
 int test[MAXSIZE];
 int n,m,k;
+int in[MAXSIZE];
+int in_cp[MAXSIZE];
+
+void init()
+{
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++){
+            s[i][j] = INF;
+            d[i][j] = INF;
+        }
+}
 
 void input()
 {
     scanf("%d %d",&n,&m);
     int t1,t2,tmp_s,tmp_d;
+    init();
     for(int i=0;i<m;i++)
     {
         scanf("%d %d %d %d",&t1,&t2,&tmp_s,&tmp_d);
         s[t1][t2]=tmp_s;
         d[t1][t2]=tmp_d;
+        in[t2]++;
+        in_cp[t2]++;
     }
     scanf("%d",&k);
     for(int i=0;i<k;i++)
         scanf("%d",&test[i]);
 }
 
+void output()
+{
+    if(judge())
+        printf("Okay.\n");
+    else
+        printf("Impossible.\n");
+    for(int i=0; i<k;i++)
+    {
+        if (in[test[i]]==0)
+            printf("You may take test %d directly.\n",test[i]);
+        else
+            output_path(test[i]);
+    }
+}
 
 #ifdef DEBUG
 void debugforinput()
@@ -37,7 +69,7 @@ void debugforinput()
             printf("%d ",s[i][j]);
         }
     }
-    printf("d matrix is:\n");
+    printf("\nd matrix is:\n");
     for(int i=0;i<n;i++)
     {
         printf("\n");
@@ -46,7 +78,7 @@ void debugforinput()
             printf("%d ",d[i][j]);
         }
     }
-    printf("test list is:\n");
+    printf("\ntest list is:\n");
     for(int i=0;i<k;i++)
     {
         printf("%d ",test[i]);
@@ -54,15 +86,31 @@ void debugforinput()
 }
 #endif
 
-int judge(int t1, int t2)
+int judge()
 {
-    
+    queue q;
+    for(int i=0;i<n;i++)
+    {
+        if(in_cp[i]==0)
+            push(q,i);
+        int cnt=0;
+        while(!is_empty(q))
+        {
+            int now=front(q);
+            cnt++;
+            pop(q);
+            for(int i=0;i<)
+        }
+    }
 }
 
 int main()
 {
     input();
+
 #ifdef DEBUG
     debugforinput();    
 #endif
+
+    return 0;
 }
---
+++ DataStructure/Project/Project3/queue.h
@@ -0,0 +1,40 @@
+typedef struct{
+    int data[MAXSIZE];
+    int top;
+    int rear;
+}queue;
+
+int is_empty(queue q)
+{
+    return (q.top==q.rear);
+}
+
+int front(queue q)
+{
+    return q.data[q.top];
+}
+
+int pop(queue q)
+{
+    if (is_empty(q)){
+        return -1;
+    }
+    else
+    {
+        q.top++;
+        return q.data[q.top-1];
+    }
+}
+
+int push(queue q, int n)
+{
+    if(q.rear>=MAXSIZE-1){
+        return -1;
+    }
+    else
+    {
+        q.rear++;
+        q.data[q.rear]=n;
+        return 1;
+    }
+}
