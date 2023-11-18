b961814a8b574e38bc16c0dc403280ce1dc92a09
YunfanLi_Ubuntu <leemakima@gmail.com>
2023-11-18 20:09:05 +0800

    ds hw1 comp

--- DataStructure/New/Hw/hw1_balloon_popping.c
+++ DataStructure/New/Hw/hw1_balloon_popping.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
+int main()
+{
+	// input data
 	int n, h;
 	scanf("%d %d", &n, &h);
-	int *balloons=(int*)malloc(n*sizeof(int));
-	for(int i=0;i<n;i++){
+	int *balloons = (int *)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+	{
 		scanf("%d", &balloons[i]);
 	}
 
-	int maxcnt=0;
-	int maxtmp=0;
-	for(int foot=0;foot<n;foot++){
-		int head=balloons[foot]+h;
-		int lastone=head;
-		while(balloons[lastone]<=h+balloons[foot])	lastone++;
-		maxtmp=lastone-foot;
-		maxcnt=(maxtmp>maxcnt)?maxcnt:maxtmp;
+	int maxCount = 0;
+	int bestPositon = 0;
+	int smallestBestPosition=0;
+	// start from balloons[0] to balloons[n]
+	for (int i = 0; i < n; i++)
+	{
+		int headPositon = balloons[i] + h;
+		int lastBalloon = i;
+		while (lastBalloon < n && balloons[lastBalloon] <= headPositon)
+			lastBalloon++;
+		int count = lastBalloon - i;
+		//update maximum count and best position
+		if (count > maxCount)
+		{
+			maxCount = count;
+			bestPositon = i;
+			smallestBestPosition=balloons[bestPositon];
+
+			//adjust to find the smallest position to satisfy the conditions
+			while(smallestBestPosition+h>balloons[bestPositon+i])
+			{
+				smallestBestPosition--;
+			}
+				
+			
+		}
 	}
+	
+	
+
+	printf("%d %d", smallestBestPosition, maxCount);
 
 	free(balloons);
 	return 0;
 }
-
