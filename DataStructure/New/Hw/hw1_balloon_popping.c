0955b96b368a7ef6dd7d4a33edfaf01ba13c5605
YunfanLi_Ubuntu <leemakima@gmail.com>
2023-11-18 13:20:55 +0800

    ds hw1

--- DataStructure/New/Hw/hw1_balloon_popping.c
+++ DataStructure/New/Hw/hw1_balloon_popping.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
-int main() {}
+#include <stdlib.h>
+int main(){
+	int n, h;
+	scanf("%d %d", &n, &h);
+	int *balloons=(int*)malloc(n*sizeof(int));
+	for(int i=0;i<n;i++){
+		scanf("%d", &balloons[i]);
+	}
+
+	int maxcnt=0;
+	int maxtmp=0;
+	for(int foot=0;foot<n;foot++){
+		int head=balloons[foot]+h;
+		int lastone=head;
+		while(balloons[lastone]<=h+balloons[foot])	lastone++;
+		maxtmp=lastone-foot;
+		maxcnt=(maxtmp>maxcnt)?maxcnt:maxtmp;
+	}
+
+	free(balloons);
+	return 0;
+}
+
